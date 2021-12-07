9184e3e1d477ccc359e7800488e65c8ef0bb97b6
Aguiar575 <aguiarus99arthurus@gmail.com>
2021-12-06 22:09:52 -0300

    basic behave functions added in UserController;

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,6 +11,21 @@ class UserController {
     const user = await User.create(req.body)
     return res.json(user)
   }
+
+  public async show (req: Request, res: Response): Promise<Response> {
+    const user = await User.findById(req.params.id)
+    return res.json(user)
+  }
+
+  public async update (req: Request, res: Response): Promise<Response> {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    return res.json(user)
+  }
+
+  public async destroy (req: Request, res: Response): Promise<Response> {
+    await User.findByIdAndDelete(req.params.id)
+    return res.send()
+  }
 }
 
 export default new UserController()
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,11 @@ import UserController from './controllers/UserController'
 
 const routes = Router()
 
+// user routes
 routes.get('/users', UserController.index)
 routes.post('/users', UserController.store)
+routes.get('/users/:id/show', UserController.show)
+routes.post('/users/:id/update', UserController.update)
+routes.post('/users/:id/delete', UserController.destroy)
 
 export default routes
